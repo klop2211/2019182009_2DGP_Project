2043df52c2981d260767af60e318939241331454
klop2211 <58390003+klop2211@users.noreply.github.com>
2022-11-26 15:41:41 +0900

    logo_state 구현

--- Final/Mygame.py
+++ Final/Mygame.py
@@ -1,7 +1,8 @@
 from pico2d import*
 import game_framework
 import play_state
+import logo_state
 
 open_canvas()
-game_framework.run(play_state)
+game_framework.run(logo_state)
 close_canvas()
--- Final/UI_object.py
+++ Final/UI_object.py
@@ -1,5 +1,8 @@
 from pico2d import *
 
+import game_framework
+import play_state
+
 class Minimap:
     def __init__(self, num = 0):
         self.image = load_image('./Resource\\UI\\minimap_sheet.png')
@@ -15,3 +18,55 @@ class Minimap:
 
     def update(self):
         pass
+
+
+class Startbutton:
+    def __init__(self):
+        self.off = load_image('./Resource\\UI\\PlayOff_kor.png')
+        self.on = load_image('./Resource\\UI\\PlayOn_kor.png')
+        self.state = 'off'
+
+    def draw(self):
+        if self.state == 'off':
+            self.off.clip_draw(0, 0, self.off.w, self.off.h, 400, 200 - 18, 108, 36)
+        else:
+            self.on.clip_draw(0, 0, self.off.w, self.off.h, 400, 200 - 18, 108, 36)
+
+    def update(self):
+        pass
+
+    def handle_event(self, event):
+        if event.type == SDL_MOUSEMOTION:
+            if event.x < 454 and event.x > 346 and 600 - event.y < 218 and 600 - event.y > 172:
+                self.state = 'on'
+            else:
+                self.state = 'off'
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            if event.x < 454 and event.x > 346 and 600 - event.y < 218 and 600 - event.y > 172:
+                game_framework.change_state(play_state)
+
+
+class Exitbutton:
+    def __init__(self):
+        self.off = load_image('./Resource\\UI\\ExitOff_kor.png')
+        self.on = load_image('./Resource\\UI\\ExitOn_kor.png')
+        self.state = 'off'
+
+    def draw(self):
+        if self.state == 'off':
+            self.off.clip_draw(0, 0, self.off.w, self.off.h, 400, 150 - 18, 63, 36)
+        else:
+            self.on.clip_draw(0, 0, self.off.w, self.off.h, 400, 150 - 18, 63, 36)
+
+    def update(self):
+        pass
+
+    def handle_event(self, event):
+        if event.type == SDL_MOUSEMOTION:
+            if event.x < 431 and event.x > 369 and 600 - event.y < 158 and 600 - event.y > 122:
+                self.state = 'on'
+            else:
+                self.state = 'off'
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            if event.x < 431 and event.x > 369 and 600 - event.y < 158 and 600 - event.y > 122:
+                game_framework.quit()
