226b6894165f33cf3687debd4953c99081f7c99e
klop2211 <58390003+klop2211@users.noreply.github.com>
2022-12-03 16:40:52 +0900

    Update Monster_object.py

--- Final/Monster_object.py
+++ Final/Monster_object.py
@@ -2,8 +2,6 @@
 import play_state
 
 
-
-
 class Monster:
     def set_dir(self):
         if self.x - play_state.hero.x > 15 or self.x - play_state.hero.x < -15:
@@ -11,7 +9,13 @@ class Monster:
                 self.dir = -1
             else:
                 self.dir = 1
-    pass
+
+    def draw_hp(self, x, y):
+        sx, sy = self.x + x, self.y + y
+        self.hp_back.draw(sx + self.w // 2, sy + self.h // 2 - 40)
+        self.hp_bar.draw(sx + self.w // 2 + 1 - int(self.hp_bar.w // 2 * (100 - self.hp) / 100),
+                         sy + self.h // 2 - 1 - 40, int(self.hp_bar.w * self.hp / 100),
+                         self.hp_bar.h)
 
 
 
